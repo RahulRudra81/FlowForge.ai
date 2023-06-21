22f668d1061ae3e75a82147e5bfa92cb7108ec47
Rahul Rudra <rudrarahul81@gmail.com>
2023-06-21 06:31:17 +0530

    ui changes on landing page login sign up authentication done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,20 @@
-import { React, useState } from 'react'
+import { React, useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { FiSun } from 'react-icons/fi'
 import DnDFlow from './DnDFlow'
 import robot from '../assets/robot.png'
+import ml from '../assets/ml.gif'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 const Landing = () => {
     const [darkMode, setDarkMode] = useState(false)
-
+    useEffect(()=>{
+        AOS.init({duration:2000})
+    },[])
     return (
-        <div className='bg-primary'>
+        <div className='bg-gradient-to-r to-gray-900 from-primary'>
 
 
             {/* Navbar */}
@@ -28,41 +34,34 @@ const Landing = () => {
 
             {/* Cotent Area */}
 
-            <div className='flex md:flex-row  flex-col sm:py-16 py-6'>
+            <div className=' flex md:flex-row  flex-col sm:py-16 py-6'>
                 <div className='flex-1 flex-col xl:px-0 sm:px-16 px-6 text-white mt-0 flex justify-center items-start w-full'>
-                    <div>
-                        <h1 className=' text-4xl md:text-6xl font-serif font-bold mb-12'>
-                            No-code<br /> <span className='text-gradient'>Audio/Video AI Apps </span><br />in seconds
+                    <div  >
+                        <h1  className=' text-4xl md:text-6xl font-serif font-bold mb-12'>
+                            No-code<br /> <span className='text-gradient' data-aos='slide-down'>Audio/Video AI Apps </span><br />in seconds
                         </h1>
                         <p className='font-serif font-bold text-[18px] max-w-[470px] mt-5 text-dimWhite'>Create AI backends in using drag and drop, which you can connect with no-code applicatoins or call with an API call</p>
                     </div>
                 </div>
                 <div className={`flex-1 flex justify-center items-center md:my-0 my-10 relative`}>
-                    <img src={robot} alt="" className="w-[100%] h-[100%] relative z-[5]" />
-
-                    {/* gradient start */}
-                    <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-                    <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-                    <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-                    {/* gradient end */}
+                    <img src={ml} alt="" className=" relative z-[5] mix-blend-screen" />
                 </div>
             </div>
 
             {/* Horizontal Line */}
 
-            <div className='flex item-center justify-center'>
+            <div className=' flex item-center justify-center'>
                 <div className="relative w-3/4  flex py-5 items-center mt-20">
-                    <div className="flex-grow border-t border-gray-400"></div>
-                    <span className="flex-shrink mx-4 text-gray-400">Preview</span>
-                    <div className="flex-grow border-t border-gray-400"></div>
+                    <div className="flex-grow border-t border-[#33bbcf]"></div>
+                    <span className="flex-shrink mx-4 text-[#33bbcf]">Preview</span>
+                    <div className="flex-grow border-t border-[#33bbcf]"></div>
                 </div>
             </div>
 
-
             {/* DnDFlow */}
 
-            <div className='flex align-middle justify-center mt-20'>
-                <div className=' hover:scale-110 w-3/4 border-solid border-8 blorder-sky-500 hover:shadow-blue-400 hover:shadow-xl'>
+            <div className='flex align-middle justify-center' data-aos='slide-right'>
+                <div className='mt-20 hover:scale-110 w-3/4 border-solid border-8 blorder-sky-500 hover:shadow-blue-400 hover:shadow-xl'>
                     <DnDFlow />
                 </div>
             </div>
