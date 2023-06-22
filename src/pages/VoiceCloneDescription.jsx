f00f9dfa8836c11e99e80796187ca7cb5e355d81
Aarize29 <1ds21ai045@dsce.edu.in>
2023-06-22 23:31:42 +0530

    Added Functionality in RightMenuBar

--- src/pages/VoiceCloneDescription.jsx
+++ src/pages/VoiceCloneDescription.jsx
@@ -4,7 +4,7 @@ import React,{useState} from 'react'
 export default () => {
 
    
-    const [imageUpload, setImageUpload] = useState(null)
+    const [audioUpload, setAudioUpload] = useState(null)
     const [description, setDescription] = useState('')
     // const [base64, setBase64] = useState(null)
     // const [form,setForm]=useState({
@@ -15,7 +15,7 @@ export default () => {
     // console.log(imageUpload)
   const handleUpload = (e) => {
     const file=e.target.files[0]
-    setImageUpload(file);
+    setAudioUpload(file);
 
     /*Important*/ 
 
@@ -32,41 +32,26 @@ export default () => {
     }
 
 
-    const handleDescription = (e) => {
-        setDescription(e.target.value)
-    }
-
     const handleFileSubmit = (e) => {
         e.preventDefault()
-        console.log(description)
-        console.log(imageUpload)
+        console.log(audioUpload)
     }
     
 
     return (
         <div className='overflow-y-auto rounded-2xl shadow-xl flex flex-col justify-center items-center shadow-gray-200'>
-            <h1 className=' text-xl font-semibold text-gray-500 m-5'>Give Data and Description</h1>
+            <h1 className=' text-xl font-semibold text-gray-500 m-5'>Give Data </h1>
           <form form onSubmit={handleFileSubmit}>
-            <div className='p-2 border-solid border-[#dcdcdc] border-2 m-2 rounded-2xl '>
-
-                <div class="max-w-xs w-[200px] rounded overflow-hidden  ">
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Discription</div>
-                        <div className="flex-grow mt-5 mb-5  border-t-2 border-solid border-zinc-300"></div>
-                        <textarea type="text" placeholder='Breifly describe' className='w-full outline-none border-2 border-solid border-zinc-300 p-2 rounded-md mb-2' value={description} onChange={handleDescription} />
-
-                    </div>
-                </div>
-            </div>
+            
             <div className=' p-2 border-solid border-[#dcdcdc] border-2 m-2 rounded-2xl '>
                 <div class="max-w-xs w-[200px] rounded overflow-hidden">
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Data</div>
+                        <div class="font-bold text-xl mb-2">Audio</div>
                         <div className="flex-grow mt-5 mb-5  border-t-2 border-solid border-zinc-300"></div>
                        
                        <label htmlFor="data" className="cursor-pointer">
                     <div className='mt-3 text-white bg-[#6469ff] hover:text-[#6469ff] hover:bg-blue-200 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>Upload</div>
-                    <input type="file"  name="data" id="data" className="sr-only" onChange={handleUpload} />
+                    <input type="file"   accept="audio/mp3,audio/*;capture=microphone"  name="data" id="data" className="sr-only" onChange={handleUpload} />
                 </label>
                        
                     </div>
