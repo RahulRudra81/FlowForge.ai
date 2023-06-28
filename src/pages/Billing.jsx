8bd050f81d3121b99fd10c510f2735293353d87b
Aarize29 <1ds21ai045@dsce.edu.in>
2023-06-28 20:27:37 +0530

    billing page update

--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import Rightmenubar from '../components/Rightmenubar'
 import Sidebar from '../components/Sidebar'
 import { auth } from '../firebase'
+import Footer from '../components/Footer'
 
 const Billing = () => {
   const [session, setSession] = useState(null)
@@ -17,15 +18,31 @@ const Billing = () => {
 
   }, [])
   return (
-    <div>
+    <div className=''>
     <Navbar/>
-    <div className="flex flex-row">
-    <Sidebar/>
-    <div className=" mx-auto lg:px-4 px-2 py-4 lg:py-8 w-full">
-      <h1>Billing</h1>
+    <div className="  flex  items-center justify-center">
+      <div className="bg-white p-8 mb-20">
+        <h1 className="text-3xl text-center font-semibold mb-6">Billing</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+            <h2 className="text-lg font-semibold mb-4">Basic Plan</h2>
+            <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
+          </div>
+          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+            <h2 className="text-lg font-semibold mb-4">Pro Plan</h2>
+            <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
+          </div>
+          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+            <h2 className="text-lg font-semibold mb-4">Enterprise Plan</h2>
+            <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <Rightmenubar/>
-    </div>
+    <Footer className='mt-20'/>
     </div>
   )
 }
