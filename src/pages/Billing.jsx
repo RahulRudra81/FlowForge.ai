8aefb9d6cbd33d2d4b38795b4c1d39845e9f8bb6
Aarize29 <1ds21ai045@dsce.edu.in>
2023-06-28 22:27:40 +0530

    billing background

--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -24,17 +24,17 @@ const Billing = () => {
       <div className="bg-white p-8 mb-20">
         <h1 className="text-3xl text-center font-semibold mb-6">Billing</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+          <div className="bg-gray-100 rounded-lg p-6 flex flex-col justify-between">
             <h2 className="text-lg font-semibold mb-4">Basic Plan</h2>
             <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
           </div>
-          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+          <div className="bg-gray-100 rounded-lg p-6 flex flex-col justify-between">
             <h2 className="text-lg font-semibold mb-4">Pro Plan</h2>
-            <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className="text-gray-700 mb-4">Lorem ipsum lorem20 dolor sit amet, consectetur adipiscing elit.</p>
             <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
           </div>
-          <div className="bg-gray-200 rounded-lg p-6 flex flex-col justify-between">
+          <div className="bg-gray-100 rounded-lg p-6 flex flex-col justify-between">
             <h2 className="text-lg font-semibold mb-4">Enterprise Plan</h2>
             <p className="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <button className="text-white justify-center h-8 w-fit bg-black p-5 text-md rounded-full flex  items-center  shadow-gray-400 shadow-md hover:shadow-lg hover:shadow-gray-400 hover:bg-gray-800">Select</button>
